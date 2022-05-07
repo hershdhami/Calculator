7ce240dbdf42ab7d245bf134e3c49b6b2da4e00d
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-05-07 12:34:52 -0400

    commit on 'scorecard'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
+import DigitButton from './DigitButton';
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, {type, payload}) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand || ""}${payload.digit}`
+      }
+    case ACTIONS.CLEAR:
+
+  }
+}
 
 function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
+
   return (
     <div className='calculator-grid'> 
       <div className='output'>
-        <div className='previous-operand'></div>
-        <div className='current-operand'></div>
+        <div className='previous-operand'> {previousOperand} {operation}</div>
+        <div className='current-operand'> {currentOperand} </div>
       </div>
       <button className='span-two'>AC</button>
       <button>DEL</button>
-      <button>÷</button>
+      <DigitButton digit="÷" dispatch={dispatch} />
+      {/* <button>÷</button> */}
       <button>1</button>
       <button>2</button>
       <button>3</button>
