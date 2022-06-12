fe6c246682efeebfb22c5b1c042f86e93876c452
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-06-12 17:51:10 -0400

    Added Delete_Digit

--- src/App.js
+++ src/App.js
@@ -51,6 +51,21 @@ function reducer(state, {type, payload}) {
         operation: payload.operation,
         currentOperand: null
       }
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null
+        }
+      }
+      if (state.currentOperand == null) return state
+      if (state.currentOperand.length === 1) return {...state, currentOperand: null}
+      
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1) 
+      }
     case ACTIONS.CLEAR:
       return {}
     case ACTIONS.EVALUATE:
